d9d3f77f281b8181b96914b348f84516d6ba984e
Documenter.jl <documenter@juliadocs.github.io>
2023-06-29 22:42:42 +0000

    build based on 84d8703

--- previews/PR448/tutorials/porous_media.jl
+++ previews/PR448/tutorials/porous_media.jl
@@ -12,17 +12,20 @@ function Elastic(;E=20.e3, ν=0.3)
     return Elastic(2G*I4dev + K*I4vol)
 end
 
-function element_routine!(Ke, fext, material::Elastic, cv, cell, args...)
+function element_routine!(Ke, re, material::Elastic, cv, cell, a, args...)
     reinit!(cv, cell)
     n_basefuncs = getnbasefunctions(cv)
-    dσdϵ = material.C
+
     for q_point in 1:getnquadpoints(cv)
         dΩ = getdetJdV(cv, q_point)
+        ϵ = function_symmetric_gradient(cv, q_point, a)
+        σ = material.C ⊡ ϵ
         for i in 1:n_basefuncs
             δ∇N = shape_symmetric_gradient(cv, q_point, i)
+            re[i] += (δ∇N ⊡ σ)*dΩ
             for j in 1:n_basefuncs
                 ∇N = shape_symmetric_gradient(cv, q_point, j)
-                Ke[i, j] += δ∇N ⊡ dσdϵ ⊡ ∇N * dΩ
+                Ke[i, j] += (δ∇N ⊡ material.C ⊡ ∇N) * dΩ
             end
         end
     end
@@ -30,55 +33,60 @@ end
 
 struct PoroElastic{T}
     elastic::Elastic{T} ## Skeleton stiffness
-    k::T            ## Permeability of liquid   [mm^4/(Ns)]
-    ϕ::T            ## Porosity                 [-]
-    K_liquid::T     ## Liquid bulk modulus      [MPa]
+    k::T     ## Permeability of liquid   [mm^4/(Ns)]
+    ϕ::T     ## Porosity                 [-]
+    α::T     ## Biot's coefficient       [-]
+    β::T     ## Liquid compressibility   [1/MPa]
 end
-PoroElastic(;elastic, k, ϕ, K_liquid) = PoroElastic(elastic, k, ϕ, K_liquid) # Keyword constructor
+PoroElastic(;elastic, k, ϕ, α, β) = PoroElastic(elastic, k, ϕ, α, β)
 
-function element_routine!(Ke, fext, m::PoroElastic, cvs::Tuple, cell, a_old, Δt, sdh)
+function element_routine!(Ke, re, m::PoroElastic, cvs::Tuple, cell, a, a_old, Δt, sdh)
     # Setup cellvalues and give easier names
     reinit!.(cvs, (cell,))
     cv_u, cv_p = cvs
+    dr_u = dof_range(sdh, :u)
+    dr_p = dof_range(sdh, :p)
 
-    # Check that cellvalues are compatible with each other (should have same quadrature rule)
-    @assert getnquadpoints(cv_u) == getnquadpoints(cv_p)
-
-    C_el = m.elastic.C ## Elastic stiffness
+    C = m.elastic.C ## Elastic stiffness
 
     # Assemble stiffness and force vectors
     for q_point in 1:getnquadpoints(cv_u)
         dΩ = getdetJdV(cv_u, q_point)
+        p = function_value(cv_p, q_point, a, dr_p)
+        p_old = function_value(cv_p, q_point, a_old, dr_p)
+        pdot = (p - p_old)/Δt
+        ∇p = function_gradient(cv_p, q_point, a, dr_p)
+        ϵ = function_symmetric_gradient(cv_u, q_point, a, dr_u)
+        tr_ϵ_old = function_divergence(cv_u, q_point, a_old, dr_u)
+        tr_ϵ_dot = (tr(ϵ) - tr_ϵ_old)/Δt
+        σ_eff = C ⊡ ϵ
         # Variation of u_i
-        for (iᵤ, Iᵤ) in pairs(dof_range(sdh, :u))
+        for (iᵤ, Iᵤ) in pairs(dr_u)
             ∇δNu = shape_symmetric_gradient(cv_u, q_point, iᵤ)
             div_δNu = shape_divergence(cv_u, q_point, iᵤ)
-            for (jᵤ, Jᵤ) in pairs(dof_range(sdh, :u))
+            re[Iᵤ] += (∇δNu ⊡ σ_eff - div_δNu*p*m.α) * dΩ
+            for (jᵤ, Jᵤ) in pairs(dr_u)
                 ∇Nu = shape_symmetric_gradient(cv_u, q_point, jᵤ)
-                Ke[Iᵤ, Jᵤ] -= ∇δNu ⊡ C_el ⊡ ∇Nu * dΩ
+                Ke[Iᵤ, Jᵤ] += (∇δNu ⊡ C ⊡ ∇Nu) * dΩ
             end
-            for (jₚ, Jₚ) in pairs(dof_range(sdh, :p))
+            for (jₚ, Jₚ) in pairs(dr_p)
                 Np = shape_value(cv_p, q_point, jₚ)
-                Ke[Iᵤ, Jₚ] += div_δNu * Np
+                Ke[Iᵤ, Jₚ] -= (div_δNu * m.α * Np) * dΩ
             end
         end
         # Variation of p_i
-        for (iₚ, Iₚ) in pairs(dof_range(sdh, :p))
+        for (iₚ, Iₚ) in pairs(dr_p)
             δNp = shape_value(cv_p, q_point, iₚ)
             ∇δNp = shape_gradient(cv_p, q_point, iₚ)
-            for (jᵤ, Jᵤ) in pairs(dof_range(sdh, :u))
+            re[Iₚ] += (δNp * (m.α * tr_ϵ_dot + m.β*pdot) + m.k * (∇δNp ⋅ ∇p) ) * dΩ
+            for (jᵤ, Jᵤ) in pairs(dr_u)
                 div_Nu = shape_divergence(cv_u, q_point, jᵤ)
-                Lpu_ij = δNp*div_Nu*dΩ
-                Ke[Iₚ,Jᵤ] += Lpu_ij
-                fext[Iₚ] += Lpu_ij*a_old[Jᵤ]
+                Ke[Iₚ,Jᵤ] += δNp*(m.α/Δt)*div_Nu*dΩ
             end
-            for (jₚ, Jₚ) in pairs(dof_range(sdh, :p))
+            for (jₚ, Jₚ) in pairs(dr_p)
                 ∇Np = shape_gradient(cv_p, q_point, jₚ)
                 Np = shape_value(cv_p, q_point, jₚ)
-                Kpp_ij = (m.k/m.ϕ) * ∇δNp ⋅ ∇Np * dΩ
-                Lpp_ij = δNp*Np/m.K_liquid
-                Ke[Iₚ,Jₚ] += Δt*Kpp_ij + Lpp_ij
-                fext[Iₚ] += Lpp_ij*a_old[Jₚ]
+                Ke[Iₚ,Jₚ] += (δNp * m.β*Np/Δt + m.k * (∇δNp ⋅ ∇Np) ) * dΩ
             end
         end
     end
@@ -90,28 +98,30 @@ struct FEDomain{M,CV,SDH<:SubDofHandler}
     sdh::SDH
 end
 
-function doassemble!(K, f, domains::Vector{<:FEDomain}, a_old, Δt)
-    assembler = start_assemble(K, f)
+function doassemble!(K, r, domains::Vector{<:FEDomain}, a, a_old, Δt)
+    assembler = start_assemble(K, r)
     for domain in domains
-        doassemble!(assembler, domain, a_old, Δt)
+        doassemble!(assembler, domain, a, a_old, Δt)
     end
 end
 
-function doassemble!(assembler, domain::FEDomain, a_old, Δt)
+function doassemble!(assembler, domain::FEDomain, a, a_old, Δt)
     material = domain.material
     cv = domain.cellvalues
     sdh = domain.sdh
     n = ndofs_per_cell(sdh)
     Ke = zeros(n,n)
-    fe = zeros(n)
+    re = zeros(n)
     ae_old = zeros(n)
-
+    ae = zeros(n)
     for cell in CellIterator(sdh)
-        map!(i->a_old[i], ae_old, celldofs(cell)) # copy values from a_old to ae_old
+        # copy values from a to ae
+        map!(i->a[i], ae, celldofs(cell))
+        map!(i->a_old[i], ae_old, celldofs(cell))
         fill!(Ke, 0)
-        fill!(fe, 0)
-        element_routine!(Ke, fe, material, cv, cell, ae_old, Δt, sdh)
-        assemble!(assembler, celldofs(cell), Ke, fe)
+        fill!(re, 0)
+        element_routine!(Ke, re, material, cv, cell, ae, ae_old, Δt, sdh)
+        assemble!(assembler, celldofs(cell), Ke, re)
     end
 end
 
@@ -127,21 +137,21 @@ function get_grid()
     return grid
 end
 
-function setup_problem(;t_rise=0.1, p_max=100.0)
+function setup_problem(;t_rise=0.1, u_max=-0.1)
 
     grid = get_grid()
 
-    # Setup the materials
+    # Define materials
     m_solid = Elastic(;E=20.e3, ν=0.3)
-    m_porous = PoroElastic(;elastic=Elastic(;E=10e3, ν=0.3), K_liquid=15e3, k=5.0e-2, ϕ=0.8)
+    m_porous = PoroElastic(;elastic=Elastic(;E=10e3, ν=0.3), β=1/15e3, α=0.9, k=5.0e-3, ϕ=0.8)
 
-    # Setup the interpolation and integration rules
-    dim=Ferrite.getdim(grid)
+    # Define interpolations
     ipu_quad = Lagrange{RefQuadrilateral,2}()^2
     ipu_tri  = Lagrange{RefTriangle,2}()^2
     ipp_quad = Lagrange{RefQuadrilateral,1}()
     ipp_tri  = Lagrange{RefTriangle,1}()
 
+    # Quadrature rules
     qr_quad = QuadratureRule{RefQuadrilateral}(2)
     qr_tri  = QuadratureRule{RefTriangle}(2)
 
@@ -177,11 +187,16 @@ function setup_problem(;t_rise=0.1, p_max=100.0)
                FEDomain(m_porous, (cvu_tri, cvp_tri), sdh_porous_tri)
                ]
 
-    # Add boundary conditions
+    # Boundary conditions
+    # Sliding for u, except top which is compressed
+    # Sealed for p, except top with prescribed zero pressure
+    addfaceset!(dh.grid, "sides", x -> x[1] < 1e-6 || x[1] ≈ 5.0)
+    addfaceset!(dh.grid, "top", x -> x[2]≈10.0)
     ch = ConstraintHandler(dh);
-    add!(ch, Dirichlet(:u, getfaceset(grid, "bottom"), (x, t) -> zero(Vec{2}), [1,2]))
-    add!(ch, Dirichlet(:p, getfaceset(grid, "bottom_p"), (x, t) -> 0.0))
-    add!(ch, Dirichlet(:p, getfaceset(grid, "top_p"), (x, t) -> p_max*clamp(t/t_rise,0,1)))
+    add!(ch, Dirichlet(:u, getfaceset(grid, "bottom"), (x, t) -> zero(Vec{1}), [2]))
+    add!(ch, Dirichlet(:u, getfaceset(grid, "sides"), (x, t) -> zero(Vec{1}), [1]))
+    add!(ch, Dirichlet(:u, getfaceset(grid, "top"), (x, t) -> u_max*clamp(t/t_rise, 0, 1), [2]))
+    add!(ch, Dirichlet(:p, getfaceset(grid, "top_p"), (x, t) -> 0.0))
     close!(ch)
 
     return dh, ch, domains
@@ -189,15 +204,20 @@ end
 
 function solve(dh, ch, domains; Δt=0.025, t_total=1.0)
     K = create_sparsity_pattern(dh);
-    f = zeros(ndofs(dh))
+    r = zeros(ndofs(dh))
     a = zeros(ndofs(dh))
+    a_old = copy(a)
     pvd = paraview_collection("porous_media.pvd");
     for (step, t) = enumerate(0:Δt:t_total)
         if t>0
-            doassemble!(K, f, domains, a, Δt)
             update!(ch, t)
-            apply!(K, f, ch)
-            a .= K\f
+            apply!(a, ch)
+            doassemble!(K, r, domains, a, a_old, Δt)
+            apply_zero!(K, r, ch)
+            Δa = -K\r
+            apply_zero!(Δa, ch)
+            a .+= Δa
+            copyto!(a_old, a)
         end
         vtk_grid("porous_media-$step", dh) do vtk
             vtk_point_data(vtk, dh, a)
